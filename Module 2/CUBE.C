4dbae36c7be8737c63ddebce63621ef80baa687c
10 June Yuvraj C,C++ <109966344+0Yuvraj0@users.noreply.github.com>
2022-08-02 18:05:05 +0530

    Add files via upload

---
+++ Module 2/CUBE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+int squre;
+long int cube;
+clrscr();
+printf("Enter the value=");
+scanf("%d",&a);
+squre=a*a;
+cube=a*a*a;
+printf("%d \n%ld",squre,cube);
+getch();
+}
