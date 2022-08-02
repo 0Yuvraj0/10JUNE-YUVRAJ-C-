1577d00bfca862d6105754fca3d1a716032e61bc
10 June Yuvraj C,C++ <109966344+0Yuvraj0@users.noreply.github.com>
2022-08-02 18:08:46 +0530

    Add files via upload

---
+++ Module 3/PRIME.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int Number;
+clrscr();
+printf("Enter the Number=",Number);
+scanf("%d",&Number);
+if
+(Number%1==0&Number%Number==0)
+{
+printf("Yes,This number is Prime Number.");
+}
+else
+{
+printf("Sorry,The Number you have enterd is not prime number.");
+}
+getch();
+
+
+}
