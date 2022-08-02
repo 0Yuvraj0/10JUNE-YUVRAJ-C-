6b12ec43d0ae7bc5b4f590c5d97f17a11a9ab6bc
10 June Yuvraj C,C++ <109966344+0Yuvraj0@users.noreply.github.com>
2022-08-02 17:58:51 +0530

    Add files via upload

---
+++ Module1/BIO.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("Name=Kubavt Yuvraj");
+printf("\nAge=16");
+printf("\nAddress=Madhapar Chowkdi,Rajkot");
+printf("\nStd=11");
+getch();
+}
